83ddde93c79bce222bb7f9a39ad64bdc6e11ce59
chrisp <christopher.p@miro.com>
2022-09-29 13:17:35 +0200

    Updated data points with new typeform questions - tos, privacy_policy, helpful links

--- typeform/typeform_data_points.py
+++ typeform/typeform_data_points.py
@@ -10,6 +10,9 @@ class typeformDataSummary():
             self.dev_email = [i['response'] for i in request_data['fields'] if config.typeform_dev_email in i['id']]
             self.short_desc = [i['response'] for i in request_data['fields'] if config.typeform_short_desc in i['id']]
             self.full_desc = [i['response'] for i in request_data['fields'] if config.typeform_full_desc in i['id']]
+            self.tos = [i['response'] for i in request_data['fields'] if config.typeform_tos in i['id']]
+            self.privacy_policy = [i['response'] for i in request_data['fields'] if config.typeform_privacy_policy in i['id']]
+            self.helpfull_links = [i['response'] for i in request_data['fields'] if config.typeform_helpfull_links in i['id']]
             self.full_condensed_typeform = request_data
         except Exception as err:
             logger.exception(err)
